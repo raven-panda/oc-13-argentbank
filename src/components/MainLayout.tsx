ae5caaf560da0b731f478f7adf214229f1ed19e1
raven-panda <ambroise.maire7@gmail.com>
2025-08-29 11:36:28 +0200

    feat(layout): created main app layout

---
+++ src/components/MainLayout.tsx
@@ -0,0 +1,31 @@
+import { Link, Outlet } from '@tanstack/react-router';
+import styles from '../assets/css/layout.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
+export default function MainLayout() {
+  return (
+    <main>
+      <NavHeader />
+      <Outlet />
+      <Footer />
+    </main>
+  );
+}
+
+function NavHeader() {
+  return (
+    <nav className={styles.mainNavHeader}>
+      <Link to="/">
+        <img src="argent-bank-logo.png" alt="Argent Bank Logo" width={200} />
+      </Link>
+      <Link to="/sign-in" className={styles.signInButton}>
+        <FontAwesomeIcon icon={faUserCircle} /> Sign In
+      </Link>
+    </nav>
+  );
+}
+
+function Footer() {
+  return <p className={styles.mainFooter}>Copyright 2020 Argent Bank</p>;
+}
